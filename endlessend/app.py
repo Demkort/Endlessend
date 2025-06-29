1f6578782728025f7f4e904a40d0db17d2551c43
Demkort <denismalgin987@gmail.com>
2025-06-29 17:23:25 +1000

    Add files via upload

---
+++ endlessend/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+app = Flask(__name__)
+
+# Авторизация в Google Sheets API
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+client = gspread.authorize(creds)
+
+# Открываем таблицу по названию
+sheet = client.open("Endlessend").sheet1  # Имя таблицы в Google Sheets
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('name')
+    lastname = request.form.get('lastName')
+    phone = request.form.get('phone')
+    email = request.form.get('email')
+
+    if not name or not lastname or not phone or not email:
+        return "Ошибка: не все поля заполнены", 400
+
+    # Добавляем строку в таблицу
+    sheet.append_row([name, lastname, phone, email])
+
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
